ffc87bb5e67eb162e5df21ad0848052f82c55650
苏政-java <851575021@qq.com>
2018-01-16 23:27:38 +0800

    java io

--- src/main/java/learn/io/FileTest.java
+++ src/main/java/learn/io/FileTest.java
@@ -1,6 +1,7 @@
 package learn.io;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 /**
@@ -16,8 +17,13 @@ public class FileTest {
         file.deleteOnExit();
         System.out.println(file.length());
         System.out.println(file.exists());
+        FilenameFilter filter = (dir, name) -> false;
+        file.list(filter);
     }
+
     public static void main(String[] args) throws IOException {
-        new FileTest().testStatic();
+        FileTest fileTest = new FileTest();
+        fileTest.testStatic();
+
     }
 }
