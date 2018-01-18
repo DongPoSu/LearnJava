0b19c8051715c287535c8e44e6f4252c2d49aeac
suzheng <851575021@qq.com>
2018-01-19 07:36:36 +0800

    fileinputstream
    test git commit amend
    
    test file inputstream

--- src/main/java/learn/io/TestIoStream.java
+++ src/main/java/learn/io/TestIoStream.java
@@ -11,20 +11,21 @@ import java.io.IOException;
  */
 public class TestIoStream {
 
-    public void testFileStream(){
+    public void testFileStream() {
 
 
         try {
-            System.out.println(System.getProperty("user.name"));
+//            System.out.println(System.getProperty("user.name"));
             File file = new File("");//设定为当前文件夹
 //            System.out.println(file.getCanonicalPath());//获取标准的路径
-            String path = file.getAbsolutePath()+"\\src\\main\\java\\learn\\io\\TestConsole.java";
+            String path = file.getAbsolutePath() + "\\src\\main\\java\\learn\\io\\TestConsole.java";
             FileInputStream inputStream = new FileInputStream(path);
             System.out.println(inputStream.available());
-            byte [] cache = new byte[1024];
-           while (inputStream.read(cache) >0){
-               System.out.println(new String(cache));
-           }
+            byte[] cache = new byte[1024];
+            while (inputStream.read(cache) > 0) {
+                System.out.println(inputStream.skip(cache.length));
+                System.out.println(new String(cache));
+            }
             inputStream.close();
 
         } catch (FileNotFoundException e) {
