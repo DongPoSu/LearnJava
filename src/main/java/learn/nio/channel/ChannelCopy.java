8d1cf2b6c8aa2c70b1a7f140adc0dcc3f6fc0348
suzheng <@Q199065>
2018-05-22 21:43:20 +0800

    新增:channel

---
+++ src/main/java/learn/nio/channel/ChannelCopy.java
@@ -0,0 +1,48 @@
+package learn.nio.channel;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * @author suzheng
+ * @version v.1.0
+ */
+public class ChannelCopy {
+
+    public static void main(String[] args) throws IOException {
+        ReadableByteChannel source = Channels.newChannel(System.in);
+        WritableByteChannel dest = Channels.newChannel(System.out);
+        channelCopy(source, dest);
+        source.close();
+        dest.close();
+    }
+
+    private static void channelCopy(ReadableByteChannel source, WritableByteChannel dest) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(16*1024);
+        while (source.read(byteBuffer) != -1){
+            byteBuffer.flip();
+            dest.write(byteBuffer);
+            byteBuffer.compact();
+        }
+
+        byteBuffer.flip();
+
+        while (byteBuffer.hasRemaining()) {
+            dest.write(byteBuffer);
+        }
+    }
+
+    private static void channelCopy2(ReadableByteChannel src, WritableByteChannel dest) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(16*1024);
+        while (src.read(byteBuffer) != -1) {
+            byteBuffer.flip();
+            while (byteBuffer.hasRemaining()) {
+                dest.write(byteBuffer);
+            }
+            byteBuffer.clear();
+        }
+    }
+}
