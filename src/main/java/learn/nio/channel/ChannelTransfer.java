32af08468b64600b14c816aafabf88d95c3f5520
suzheng <851575021@qq.com>
2018-05-23 17:49:43 +0800

    add: file channel

---
+++ src/main/java/learn/nio/channel/ChannelTransfer.java
@@ -0,0 +1,31 @@
+package learn.nio.channel;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * @author suzheng
+ * @version v.1.0
+ */
+public class ChannelTransfer {
+    public static void main(String[] args) throws IOException {
+        if(args.length ==0){
+            System.out.println("Usage: filename ...");
+            return;
+        }
+        catFiles(Channels.newChannel(System.out), args);
+    }
+
+    private static void catFiles(WritableByteChannel target, String[] args) throws IOException {
+        for (int i = 0; i < args.length; i++) {
+            FileInputStream fileInputStream = new FileInputStream(args[i]);
+            FileChannel channel  = fileInputStream.getChannel();
+            channel.transferTo(0, channel.size(),target);
+            channel.close();
+            fileInputStream.close();
+        }
+    }
+}
--- src/main/java/learn/nio/channel/FileChannelTest.java
+++ src/main/java/learn/nio/channel/FileChannelTest.java
@@ -1,12 +1,11 @@
 package learn.nio.channel;
 
-import java.io.*;
-import java.net.URI;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Path;
 
 /**
  * author: suzheng
@@ -19,13 +18,14 @@ import java.nio.file.Path;
 public class FileChannelTest {
     public static void main(String[] args) throws IOException {
         ReadableByteChannel source = Channels.newChannel(System.in);
-        FileOutputStream outputStream = new FileOutputStream(new File("d:\\test.txt"));
+        FileOutputStream outputStream = new FileOutputStream("test.txt");
         FileChannel fileChannel = outputStream.getChannel();
 //        fileChannel.transferFrom(source,0,100);
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(16*1024);
         while (source.read(byteBuffer) != -1){
             byteBuffer.flip();
             fileChannel.write(byteBuffer);
+
             byteBuffer.clear();
         }
         source.close();
